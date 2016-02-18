0b93ece6ef3038cdbe517c97e4f964c9d2e5a9fc
Jesse McBride <jm@ucf.edu>
2016-02-18 15:09:21 -0500

    Generate type-specific objects based on string response (i.e. datetime objects)

--- canvas_object.py
+++ canvas_object.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+import re
+
+DATE_PATTERN = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')
+
 class CanvasObject(object):
 	"""
 	Base class for all classes representing objects returned by the API.
@@ -14,3 +19,8 @@ class CanvasObject(object):
 		# Initialize object attributes
 		for attribute, value in attributes.iteritems():
 			self.__setattr__(attribute, value)
+
+			# Generate extra attributes (i.e. datetime attributes for dates)
+			if DATE_PATTERN.match(str(value)):
+				date = datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+				self.__setattr__(attribute + '_date', date)
