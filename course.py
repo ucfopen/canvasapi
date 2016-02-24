1cb3a4cb8f27ddda98e39a38f6664f5ba7008450
Jesse McBride <jm@ucf.edu>
2016-02-24 16:08:30 -0500

    Course#enroll_user added.

--- course.py
+++ course.py
@@ -44,17 +44,17 @@ class Course(CanvasObject):
         <https://canvas.instructure.com/doc/api/courses.html#method.courses.update>`
         :rtype: bool: True if the course was updated, False otherwise.
         """
-        try:
-            response = self._requester.request(
-                'PUT',
-                'courses/%s' % (self.id),
-                **combine_kwargs(**kwargs)
-            )
+        response = self._requester.request(
+            'PUT',
+            'courses/%s' % (self.id),
+            **combine_kwargs(**kwargs)
+        )
 
+        if response.json().has_key('name'):
             super(Course, self).set_attributes(response.json())
-            return True
-        except Exception:
-            return False
+
+        return response.json().has_key('name')
+
 
     def get_users(self, **kwargs):
         """
@@ -73,6 +73,30 @@ class Course(CanvasObject):
             **combine_kwargs(**kwargs)
         )
         return [User(self._requester, user) for user in response.json()]
+
+    def enroll_user(self, user, enrollment_type, **kwargs):
+        """
+        Create a new user enrollment for a course or a section.
+
+        :calls: `POST /api/v1/courses/:course_id/enrollments 
+                <https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.create>`
+        
+        :param user: the user to enroll
+        :type user: :class:`pycanvas.user.User`
+        :param enrollment_type: the type of enrollment
+        :type enrollment_type: str
+        :rtype: bool: True if the user was enrolled, False otherwise
+        """
+        kwargs['enrollment[user_id]'] = user.id
+        kwargs['enrollment[type]'] = enrollment_type
+
+        response = self._requester.request(
+            'POST',
+            'courses/%d/enrollments' % (self.id),
+            **combine_kwargs(**kwargs)
+        )
+
+        return response.json().has_key('course_section_id')
 
     def __str__(self):
         return "%s %s %s" % (self.id, self.course_code, self.name)
