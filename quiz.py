94dac9da736d0475d3a37690168a0e673e30c675
Elise Heron <elise.heron93@gmail.com>
2016-04-06 15:57:37 -0400

    Made some style changes

--- quiz.py
+++ quiz.py
@@ -1,10 +1,10 @@
 from canvas_object import CanvasObject
-from paginated_list import PaginatedList
 
 class Quiz(CanvasObject):
+
 
     def __str__(self):
         return "id %s, title: %s" % (
             self.id,
-            self.name
+            self.title
         )
