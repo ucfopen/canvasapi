e08f7fed6879620e60dc7274f0fe62b938eedd4d
Devin Singh <devin.singh@ucf.edu>
2016-04-07 16:39:30 -0400

    Added delete() method

--- assignment.py
+++ assignment.py
@@ -9,3 +9,20 @@ class Assignment(CanvasObject):
             self.name,
             self.description
         )
+
+    def delete(self):
+        """
+        Delete the given assignment.
+
+        :calls: `DELETE /api/v1/courses/:course_id/assignments/:id`
+        <https://canvas.instructure.com/doc/api/assignments.html#method.assignments.destroy>
+        :rtype: Assignment
+        """
+
+        response = self._requester.request(
+            'DELETE',
+            'courses/%s/assignments/%s' % (self.course_id, self.id),
+            event="delete"
+        )
+
+        return response.json()
