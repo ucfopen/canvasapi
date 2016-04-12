78a37cd3fc30ab2f43f31e46f95ab9b2431eee59
Elise Heron <elise.heron93@gmail.com>
2016-04-12 17:33:27 -0400

    Finished edit quiz and started delete quiz

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
 from canvas_object import CanvasObject
+from util import combine_kwargs
 
 class Quiz(CanvasObject):
 
@@ -9,4 +10,29 @@ class Quiz(CanvasObject):
             self.title
         )
 
+    def edit_quiz(self, course_id, **kwargs):
+        """
+        Modifies an existing quiz
+        :calls: `PUT /api/v1/courses/:course_id/quizzes/:id`
+        <https://canvas.instructure.com/doc/api/quizzes.html#method.quizzes/quizzes_api.update>
+        :rtype: Quiz
+        """
+        from course import Course
+        response = self._requester.request(
+            'PUT',
+            'courses/%s/quizzes/%s' % (course_id, self.id),
+            **combine_kwargs(**kwargs)
+        )
+        return Quiz(self._requester, response.json())
 
+    def delete_quiz(self):
+        """
+        Deletes a quiz
+        :calls: `DELETE /api/v1/courses/:course_id/quizzes/:id`
+        <https://canvas.instructure.com/doc/api/quizzes.html#method.quizzes/quizzes_api.destroy>
+        """
+        from courses import Course
+        response = self._requester.request(
+            'DELETE',
+            'courses/%s/'
+        )
