45d38b72b6a4f78bf9c154d5adf5c76aa61119cd
Devin Singh <devin.singh@ucf.edu>
2016-04-20 17:05:38 -0400

    Added create assignment method

--- course.py
+++ course.py
@@ -213,7 +213,7 @@ class Course(CanvasObject):
 
         :calls: `GET /api/v1/courses/:course_id/assignments/:id`
         <https://canvas.instructure.com/doc/api/assignments.html#method.assignments_api.show>
-        :rtype: Assignment
+        :rtype: :class:`Assignment`
         """
         from assignment import Assignment
 
@@ -223,7 +223,7 @@ class Course(CanvasObject):
         )
         return Assignment(self._requester, response.json())
 
-    def list_assignments(self):
+    def get_assignments(self):
         """
         Returns the list of assignments for the current context.
 
@@ -239,3 +239,20 @@ class Course(CanvasObject):
             'GET',
             'courses/%s/assignments' % (self.id)
         )
+
+    def create_assignment(self, **kwargs):
+        """
+        Create a new assignment for this course. The assignment is created in the active state.
+        :calls: `POST /api/v1/courses/:course_id/assignments`
+        <https://canvas.instructure.com/doc/api/assignments.html#method.assignments_api.create>
+        :rtype: :class:`Assignment`
+        """
+        from assignment import Assignment
+
+        response = self._requester.request(
+            'POST',
+            'courses/%s/assignments' % (self.id),
+            **combine_kwargs(**kwargs)
+        )
+
+        return Assignment(self._requester, response.json())
