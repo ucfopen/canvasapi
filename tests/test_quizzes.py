b99dbc7450cb0f55035759a2f4215545aac85f40
Elise Heron <elise.heron93@gmail.com>
2016-04-26 13:54:55 -0400

    adding tests

---
+++ tests/test_quizzes.py
@@ -0,0 +1,21 @@
+import unittest
+from datetime import datetime
+
+import requests_mock
+
+from pycanvas import Quiz
+
+
+class TestQuizzes(unittest.TestCase):
+	"""
+	Tests Quiz functionality
+	"""
+	@classmethod
+	def setUpClass(self):
+		requires = {
+
+		}
+
+	def test_edit_quiz(self):
+
+	def test_delete_quiz(self):
