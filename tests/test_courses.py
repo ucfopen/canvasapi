b02b8b1a40c36e57749b14bae249211dbdeb2ab2
Elise Heron <elise.heron93@gmail.com>
2016-05-02 13:12:53 -0400

    added test method for create quiz

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,9 +1,12 @@
 import unittest
 import settings
+import requests_mock
 
 from util import register_uris
 from pycanvas.course import Course
 from pycanvas.quiz import Quiz
+from pycanvas import Canvas
+
 
 class TestCourses(unittest.TestCase):
     """
@@ -12,18 +15,23 @@ class TestCourses(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         requires = {
-            'course': ['create']
+            'course': ['create', 'get_by_id'],
+            'generic': ['not_found'],
+            'quiz': ['get_by_id']
         }
 
         adapter = requests_mock.Adapter()
         self.canvas = Canvas(settings.BASE_URL, settings.API_KEY, adapter)
         register_uris(settings.BASE_URL, requires, adapter)
 
+        self.course = self.canvas.get_course(1)
+        self.quiz = self.course.get_quiz(1)
+
     def test_create_quiz(self):
-        title = 'New Title'
+        title = 'Newer Title'
         new_quiz = self.course.create_quiz(self.course.id, quiz={'title': title})
 
-        assert isinstance(new_quiz, Course)
+        assert isinstance(new_quiz, Quiz)
         assert hasattr(new_quiz, 'title')
         assert new_quiz.title == title
 
