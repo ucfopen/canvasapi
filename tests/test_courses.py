11b01daaf18b948d574958ba26173c64a1184206
Elise Heron <elise.heron93@gmail.com>
2016-05-02 13:26:36 -0400

    Added method for test get quiz fail

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -5,7 +5,10 @@ import requests_mock
 from util import register_uris
 from pycanvas.course import Course
 from pycanvas.quiz import Quiz
+from pycanvas.exceptions import ResourceDoesNotExist
 from pycanvas import Canvas
+
+INVALID_ID = 9001
 
 
 class TestCourses(unittest.TestCase):
@@ -27,6 +30,7 @@ class TestCourses(unittest.TestCase):
         self.course = self.canvas.get_course(1)
         self.quiz = self.course.get_quiz(1)
 
+    #create_quiz()
     def test_create_quiz(self):
         title = 'Newer Title'
         new_quiz = self.course.create_quiz(self.course.id, quiz={'title': title})
@@ -35,10 +39,16 @@ class TestCourses(unittest.TestCase):
         assert hasattr(new_quiz, 'title')
         assert new_quiz.title == title
 
+    #get_quiz()
     def test_get_quiz(self):
         target_quiz = self.course.get_quiz(1)
 
         assert isinstance(target_quiz, Quiz)
 
+    def test_get_quiz_fail(self):
+        with self.assertRaises(ResourceDoesNotExist):
+            self.course.get_quiz(INVALID_ID)
+
+    #list_quizzes()
     def test_list_quizzes(self):
         pass
