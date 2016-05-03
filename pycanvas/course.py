40f12f4b832cf76ab8c8a30a56e2b5ef527cb177
Elise Heron <elise.heron93@gmail.com>
2016-05-03 15:44:26 -0400

    added list enrollment for course.py

--- pycanvas/course.py
+++ pycanvas/course.py
@@ -206,3 +206,20 @@ class Course(CanvasObject):
             'courses/%s/reset_content' % (self.id),
         )
         return Course(self._requester, response.json())
+
+    def list_enrollments(self):
+        """
+        Lists all of the enrollments within a course.
+
+        :calls: `GET /api/v1/courses/:course_id/enrollments`
+        <https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.index>
+        :rtype: :class:`PaginatedList` of :class:`Enrollment`
+        """
+        from enrollment import Enrollment
+        return PaginatedList(
+            Enrollment,
+            self._requester,
+            'GET',
+            'courses/%s/enrollments' % (self.id)
+        )
+
