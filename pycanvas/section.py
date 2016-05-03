d833b392d0c5c422e0d0cd052369da8d6915df8f
Elise Heron <elise.heron93@gmail.com>
2016-05-03 16:07:37 -0400

    Finished list enrollments methods

---
+++ pycanvas/section.py
@@ -0,0 +1,26 @@
+from canvas_object import CanvasObject
+from util import combine_kwargs
+from paginated_list import PaginatedList
+
+
+class Section(CanvasObject):
+
+    def __str__(self):
+        return
+
+    def list_enrollments(self):
+        """
+        Lists all of the enrollments for a user.
+
+        :calls: `GET /api/v1/sections/:section_id/enrollments`
+        <https://canvas.instructure.com/doc/api/enrollments.html#method.enrollments_api.index>
+        :rtype: :class:`PaginatedList` of :class:`Enrollment`
+        """
+        from enrollment import Enrollment
+
+        return PaginatedList(
+            Enrollment,
+            self._requester,
+            'GET',
+            'sections/%s/enrollments' % (self.id)
+        )
