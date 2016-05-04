2b95bfd0401fddfbea177d7575b1396bdc94a312
Elise Heron <elise.heron93@gmail.com>
2016-05-04 14:49:00 -0400

    completed test for list_enrollments

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,6 +4,7 @@ import requests_mock
 
 from util import register_uris
 from pycanvas.user import User
+from pycanvas.enrollment import Enrollment
 from pycanvas.exceptions import ResourceDoesNotExist
 from pycanvas import Canvas
 
@@ -15,9 +16,17 @@ class TestUser(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         requires = {
-
+            'user': ['list_enrollments', 'list_enrollments_2'],
+            'generic': ['not_found']
         }
 
         adapter = requests_mock.Adapter()
         self.canvas = Canvas(settings.BASE_URL, settings.API_KEY, adapter)
         register_uris(settings.BASE_URL, requires, adapter)
+
+    def test_list_enrollments(self):
+        enrollments = self.user.list_enrollments()
+        enrollment_list = [enrollment for enrollment in enrollments]
+
+        assert len(enrollment_list) == 4
+        assert isinstance(enrollment_list[0], enrollment)
