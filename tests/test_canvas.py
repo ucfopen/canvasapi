86e12ae6cd05c535113485900b62f004050635bf
Elise Heron <elise.heron93@gmail.com>
2016-05-05 16:03:05 -0400

    added test for sections

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -8,6 +8,7 @@ from pycanvas.account import Account
 from pycanvas.course import Course
 from pycanvas.course_nickname import CourseNickname
 from pycanvas.exceptions import ResourceDoesNotExist
+from pycanvas.section import Section
 from pycanvas.user import User
 import settings
 from util import register_uris
@@ -25,6 +26,7 @@ class TestCanvas(unittest.TestCase):
             'account': ['create', 'domains', 'get_by_id', 'multiple', 'multiple_course'],
             'course': ['get_by_id', 'multiple', 'multiple_page_2', 'start_at_date'],
             'generic': ['not_found'],
+            'section': ['get_by_id'],
             'user': [
                 'activity_stream_summary', 'course_nickname', 'course_nickname_set',
                 'course_nicknames', 'course_nicknames_delete', 'course_nicknames_page_2',
@@ -175,3 +177,9 @@ class TestCanvas(unittest.TestCase):
         assert isinstance(domains, list)
         assert len(domains) == 1
         assert 'name' in domains[0]
+
+    # get_section()
+    def test_section(self):
+        info = self.canvas.get_section(1)
+
+        assert isinstance(info, Section)
