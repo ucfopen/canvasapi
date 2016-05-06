b5a700da6264661cbedff91b8289f1f27c84b3e1
Elise Heron <elise.heron93@gmail.com>
2016-05-06 13:29:37 -0400

    added test for list roles

---
+++ tests/test_account.py
@@ -0,0 +1,32 @@
+import unittest
+import settings
+import requests_mock
+
+from util import register_uris
+from pycanvas import Canvas
+from pycanvas.role import Role
+
+
+class TestAccount(unittest.TestCase):
+    """
+    Tests core Account functionality
+    """
+    @classmethod
+    def setUpClass(self):
+        requires = {
+            'account': ['get_by_id', 'list_roles', 'list_roles_2'],
+            'generic': ['not_found']
+        }
+
+        adapter = requests_mock.Adapter()
+        self.canvas = Canvas(settings.BASE_URL, settings.API_KEY, adapter)
+        register_uris(settings.BASE_URL, requires, adapter)
+
+        self.account = self.canvas.get_account(1)
+
+    def list_roles(self):
+        roles = self.account.list_roles()
+        role_list = [role for role in roles]
+
+        assert len(role_list) == 4
+        assert isinstance(role_list[0], Role)
