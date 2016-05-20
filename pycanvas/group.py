01ba2444857ed35376d8b5093baaaa5701d2da34
Elise Heron <elise.heron93@gmail.com>
2016-05-20 16:40:01 -0400

    added method for get a single group

--- pycanvas/group.py
+++ pycanvas/group.py
@@ -9,6 +9,23 @@ class Group(CanvasObject):
     def __str__(self):
         return "%s %s %s" % (self.id, self.name, self.description)
 
+    def get_single_group(self):
+        """
+        Return the data for a single group. If the caller does not
+        have permission to view the group a 401 will be returned.
+
+        :calls: `GET /api/v1/groups/:group_id \
+        <https://canvas.instructure.com/doc/api/groups.html#method.groups.show>`_
+
+        :rtype: :class:`pycanvas.group.Group`
+        """
+
+        response = self._requester.request(
+            'GET',
+            'groups/%s' % (self.id)
+        )
+        return Group(self._requester, response.json())
+
     def show_front_page(self):
         """
         Retrieve the content of the front page.
@@ -16,7 +33,7 @@ class Group(CanvasObject):
         :calls: `GET /api/v1/groups/:group_id/front_page \
         <https://canvas.instructure.com/doc/api/pages.html#method.wiki_pages_api.show_front_page>`_
 
-        :rtype: :class:`pycanvas.course.Course`
+        :rtype: :class:`pycanvas.group.Group`
         """
         from course import Page
 
