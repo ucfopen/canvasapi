5e225a05981d5ae3b031910aecc39efabb666a1f
Matthew Emond <me@ucf.edu>
2016-06-17 15:58:45 -0400

    Added parent_id, parent_type and get_parent methods to help move between ExternalTools and accounts/courses

--- pycanvas/external_tool.py
+++ pycanvas/external_tool.py
@@ -8,3 +8,50 @@ class ExternalTool(CanvasObject):
 
     def __str__(self):
         return "%s %s %s" % (self.id, self.name, self.description)
+
+    @property
+    def parent_id(self):
+        """
+        Return the id of the course or account that spawned this tool.
+
+        :rtype: int
+        """
+        if hasattr(self, 'course_id'):
+            return self.course_id
+        elif hasattr(self, 'account_id'):
+            return self.account_id
+        else:
+            raise ValueError("ExternalTool does not have a course_id or account_id")
+
+    @property
+    def parent_type(self):
+        """
+        Return whether the tool was spawned from a course or account.
+
+        :rtype: str
+        """
+        if hasattr(self, 'course_id'):
+            return 'course'
+        elif hasattr(self, 'account_id'):
+            return 'account'
+        else:
+            raise ValueError("ExternalTool does not have a course_id or account_id")
+
+    def get_parent(self):
+        """
+        Return the object that spawned this tool.
+
+        :rtype: :class:`pycanvas.account.Account` or :class:`pycanvas.account.Course`
+        """
+        from account import Account
+        from course import Course
+
+        response = self._requester.request(
+            'GET',
+            '%ss/%s' % (self.parent_type, self.parent_id)
+        )
+
+        if self.parent_type == 'account':
+            return Account(self._requester, response.json())
+        elif self.parent_type == 'course':
+            return Course(self._requester, response.json())
--- tests/test_external_tool.py
+++ tests/test_external_tool.py
@@ -5,6 +5,9 @@ import requests_mock
 import settings
 from util import register_uris
 from pycanvas import Canvas
+from pycanvas.account import Account
+from pycanvas.course import Course
+from pycanvas.external_tool import ExternalTool
 
 
 class TestExternalTool(unittest.TestCase):
@@ -14,8 +17,9 @@ class TestExternalTool(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         requires = {
+            'account': ['get_by_id'],
             'course': ['get_by_id'],
-            'external_tool': ['get_by_id_course'],
+            'external_tool': ['get_by_id_account', 'get_by_id_course'],
         }
 
         adapter = requests_mock.Adapter()
@@ -23,9 +27,42 @@ class TestExternalTool(unittest.TestCase):
         register_uris(settings.BASE_URL, requires, adapter)
 
         self.course = self.canvas.get_course(1)
-        self.ext_tool = self.course.get_external_tool(1)
+        self.account = self.canvas.get_account(1)
+        self.ext_tool_course = self.course.get_external_tool(1)
+        self.ext_tool_account = self.account.get_external_tool(1)
 
     # __str__()
     def test__str__(self):
-        string = str(self.ext_tool)
+        string = str(self.ext_tool_course)
         assert isinstance(string, str)
+
+    # parent_id
+    def test_parent_id_account(self):
+        assert self.ext_tool_account.parent_id == 1
+
+    def test_parent_id_course(self):
+        assert self.ext_tool_course.parent_id == 1
+
+    def test_parent_id_no_id(self):
+        tool = ExternalTool(self.canvas._Canvas__requester, {'id': 1})
+        with self.assertRaises(ValueError):
+            tool.parent_id
+
+    # parent_type
+    def test_parent_type_account(self):
+        assert self.ext_tool_account.parent_type == 'account'
+
+    def test_parent_type_course(self):
+        assert self.ext_tool_course.parent_type == 'course'
+
+    def test_parent_type_no_id(self):
+        tool = ExternalTool(self.canvas._Canvas__requester, {'id': 1})
+        with self.assertRaises(ValueError):
+            tool.parent_type
+
+    # get_parent()
+    def test_get_parent_account(self):
+        assert isinstance(self.ext_tool_account.get_parent(), Account)
+
+    def test_get_parent_course(self):
+        assert isinstance(self.ext_tool_course.get_parent(), Course)
