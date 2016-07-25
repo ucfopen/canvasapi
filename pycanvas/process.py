9526df64f3fbf408e8582493ca05e8b333655f91
Adrian Goetz <adriangoetz@ucf.edu>
2016-07-25 14:11:01 -0400

    added process.py to create the Process class, used in Conversation.batch_update()

---
+++ pycanvas/process.py
@@ -0,0 +1,6 @@
+from pycanvas.canvas_object import CanvasObject
+
+class Process(CanvasObject):
+
+    def __str__(self):  # pragma: no cover
+        return "%s: %s, %s" % (self.id, self.tag, self.workflow_state)
