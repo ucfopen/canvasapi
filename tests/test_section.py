1814b5b45d9bbf5fb38ff3d7362713fa6b14e30b
Elise Heron <elise.heron93@gmail.com>
2016-08-02 11:02:26 -0400

    Added tests for decross list section

--- tests/test_section.py
+++ tests/test_section.py
@@ -5,6 +5,7 @@ import requests_mock
 from util import register_uris
 from pycanvas.enrollment import Enrollment
 from pycanvas import Canvas
+from pycanvas import Section
 
 
 class TestSection(unittest.TestCase):
@@ -15,7 +16,7 @@ class TestSection(unittest.TestCase):
     def setUpClass(self):
         requires = {
             'generic': ['not_found'],
-            'section': ['get_by_id', 'list_enrollments', 'list_enrollments_2']
+            'section': ['decross_section', 'get_by_id', 'list_enrollments', 'list_enrollments_2']
         }
 
         adapter = requests_mock.Adapter()
@@ -36,3 +37,8 @@ class TestSection(unittest.TestCase):
 
         assert len(enrollment_list) == 4
         assert isinstance(enrollment_list[0], Enrollment)
+
+    def test_decross_list_section(self):
+        section = self.section.test_decross_list_section()
+
+        assert isinstance(section, Section)
