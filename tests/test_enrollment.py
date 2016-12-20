fe13e79cdeb44162e6f64c8f1e4389ab5906d03a
Matthew Emond <me@ucf.edu>
2016-12-20 09:02:00 -0500

    Updated test_enrollment and test_external_tool

--- tests/test_enrollment.py
+++ tests/test_enrollment.py
@@ -8,41 +8,44 @@ from tests import settings
 from tests.util import register_uris
 
 
+@requests_mock.Mocker()
 class TestEnrollment(unittest.TestCase):
-    """
-    Test Enrollment methods
-    """
-    @classmethod
-    def setUpClass(self):
-        requires = {
-            'account': ['get_by_id'],
-            'generic': ['not_found'],
-            'enrollment': ['deactivate', 'get_by_id', 'reactivate']
-        }
-        adapter = requests_mock.Adapter()
-        self.canvas = Canvas(settings.BASE_URL, settings.API_KEY, adapter)
-        register_uris(settings.BASE_URL, requires, adapter)
 
-        self.account = self.canvas.get_account(1)
-        self.enrollment = self.account.get_enrollment(1)
+    @classmethod
+    def setUp(self):
+        self.canvas = Canvas(settings.BASE_URL, settings.API_KEY)
+
+        with requests_mock.Mocker() as m:
+            requires = {
+                'account': ['get_by_id'],
+                'enrollment': ['get_by_id']
+            }
+            register_uris(requires, m)
+
+            self.account = self.canvas.get_account(1)
+            self.enrollment = self.account.get_enrollment(1)
 
     # __str__()
-    def test__str__(self):
+    def test__str__(self, m):
         string = str(self.enrollment)
         self.assertIsInstance(string, str)
 
     # deactivate()
-    def test_deactivate(self):
+    def test_deactivate(self, m):
+        register_uris({'enrollment': ['deactivate']}, m)
+
         target_enrollment = self.enrollment.deactivate('conclude')
 
         self.assertIsInstance(target_enrollment, Enrollment)
 
-    def test_deactivate_invalid_task(self):
+    def test_deactivate_invalid_task(self, m):
         with self.assertRaises(ValueError):
             self.enrollment.deactivate('finish')
 
     # reactivate()
-    def test_reactivate(self):
+    def test_reactivate(self, m):
+        register_uris({'enrollment': ['reactivate']}, m)
+
         target_enrollment = self.enrollment.reactivate()
 
         self.assertIsInstance(target_enrollment, Enrollment)
