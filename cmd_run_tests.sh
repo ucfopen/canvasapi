475f6257792b2eb74c51828d44badfdbe8d4c47c
Matthew Emond <me@ucf.edu>
2017-01-30 15:13:08 -0500

    Moved coveragerc file settings to setup.cfg. Removed unneeded imports in __init__. Added pycodestyle and pyflakes to test_requirements. Updated jenkins tests to run linters.

--- cmd_run_tests.sh
+++ cmd_run_tests.sh
@@ -3,3 +3,5 @@ set -e
 
 docker run --rm -v $(pwd):/app pycanvas coverage run -m unittest discover
 docker run --rm -v $(pwd):/app pycanvas coverage xml
+docker run --rm -v $(pwd):/app pycanvas pycodestyle pycanvas tests
+docker run --rm -v $(pwd):/app pycanvas pyflakes pycanvas tests
