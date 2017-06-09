8b20a35996d209d91f2e2a746958b0a786cc5251
Nathan Dabu <nathan.dabu@ucf.edu>
2017-06-09 13:50:46 -0400

    cleaned up functions & removed unnecessary functions/data types

--- canvasapi/communication_channel.py
+++ canvasapi/communication_channel.py
@@ -1,30 +1,11 @@
 from canvasapi.canvas_object import CanvasObject
-from canvasapi.exceptions import RequiredFieldMissing
-from canvasapi.notification_preference import NotificationPreferenceList
-from canvasapi.util import combine_kwargs
+from canvasapi.notification_preference import NotificationPreference
 
 
 class CommunicationChannel(CanvasObject):
 
     def __str__(self):
         return "{} ({})".format(self.address, self.id)
-
-    def delete(self, **kwargs):
-        """
-        Delete this communication channel.
-
-        :calls: `DELETE /api/v1/users/:user_id/communication_channels/:id \
-        <https://canvas.instructure.com/doc/api/communication_channels.html#method.communication_channels.destroy>`_
-
-        :rtype: :class:`canvasapi.communication_channel.CommunicationChannel`
-        """
-        response = self._requester.request(
-            'DELETE',
-            'users/%s/communication_channels/%s' % (self.user_id, self.id),
-            **combine_kwargs(**kwargs)
-        )
-
-        return CommunicationChannel(self._requester, response.json())
 
     def list_preferences(self):
         """
@@ -34,8 +15,7 @@ class CommunicationChannel(CanvasObject):
             /api/v1/users/:user_id/communication_channels/:cc_id/notification_preferences \
         <https://canvas.instructure.com/doc/api/notification_preferences.html#method.notification_preferences.index>`_
 
-        :rtype: :class:`canvasapi.paginated_list.PaginatedList` of
-            :class:`canvasapi.notification_preference.NotificationPreference`
+        :rtype: `list`
         """
         response = self._requester.request(
             'GET',
@@ -44,7 +24,7 @@ class CommunicationChannel(CanvasObject):
                 self.id
             )
         )
-        return NotificationPreferenceList(self._requester, response.json())
+        return response.json()['notification_preferences']
 
     def list_preference_categories(self):
         """
@@ -64,8 +44,7 @@ class CommunicationChannel(CanvasObject):
                 self.id
             )
         )
-        data = NotificationPreferenceList(self._requester, response.json())
-        return data.categories
+        return response.json['categories']
 
     def get_preference(self, notification):
         """
@@ -88,37 +67,5 @@ class CommunicationChannel(CanvasObject):
                 notification
             )
         )
-
-        # returns a dict with a list of NotificationPreferences
-        data = NotificationPreferenceList(self._requester, response.json())
-        return data.notification_preferences[0]
-
-    def update_preference(self, notification, notification_preferences, **kwargs):
-        """
-        Change the preference for a single notification for a single
-        communication channel.
-
-        :calls: `PUT
-            /api/v1/users/:u_id/communication_channels/:cc_id/notification_preferences/:notif \
-        <https://canvas.instructure.com/doc/api/notification_preferences.html#method.notification_preferences.update>`_
-
-        :rtype: :class:`canvasapi.notification_preference.NotificationPreference`
-        """
-        if isinstance(notification_preferences, dict) and 'frequency' in notification_preferences:
-            kwargs['notification_preferences'] = notification_preferences
-        else:
-            raise RequiredFieldMissing(
-                "Dictionary with key 'frequency' is required"
-            )
-
-        response = self._requester.request(
-            'PUT',
-            'users/%s/communication_channels/%s/notification_preferences/%s' % (
-                self.user_id,
-                self.id,
-                notification
-            ),
-            **combine_kwargs(**kwargs)
-        )
-
-        return response.json()
+        data = response.json()['notification_preferences'][0]
+        return NotificationPreference(self._requester, data)
--- canvasapi/notification_preference.py
+++ canvasapi/notification_preference.py
@@ -5,9 +5,3 @@ class NotificationPreference(CanvasObject):
 
     def __str__(self):
         return "{} ({})".format(self.notification, self.frequency)
-
-
-class NotificationPreferenceList(CanvasObject):
-
-    def __str__(self):
-        return "List of %d NotificationPreferences" % len(self.notification_preferences)
