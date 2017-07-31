98391695329229cece38567c57db2b7756c82ef5
Matthew Emond <me@ucf.edu>
2017-07-31 11:22:51 -0400

    Added more imports from __future__ for compatibility

--- canvasapi/login.py
+++ canvasapi/login.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from canvasapi.canvas_object import CanvasObject
 from canvasapi.util import combine_kwargs
--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 BASE_URL = 'http://example.com/api/v1/'
 API_KEY = '123'
