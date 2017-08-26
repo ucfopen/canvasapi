9ec64a64ba930303caa7a616693a4c9446805943
Ben Liblit <liblit@cs.wisc.edu>
2017-08-26 16:50:19 -0500

    Expect items in ordered lists to be consecutively numbered
    By default, Markdown lint expects every ordered-list item to begin
    with "1.":
    <https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#md029---ordered-list-item-prefix>.
    Here, however, they are numbered consecutively: "1.", "2.", "3.", etc.

--- markdown-style.rb
+++ markdown-style.rb
@@ -4,4 +4,5 @@ exclude_rule 'line-length'
 exclude_rule 'no-duplicate-header'
 
 rule 'no-trailing-punctuation', :punctuation => '.,;:!'
+rule 'ol-prefix', :style => 'ordered'
 rule 'ul-indent', :indent => 4
