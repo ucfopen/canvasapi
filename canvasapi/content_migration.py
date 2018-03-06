b3c757d9ad51457575249ba883109207e2b6e9b5
Elli Howard <elise.howard@ucf.edu>
2018-03-06 13:56:45 -0500

    Cleaned up content migration for readability

--- canvasapi/content_migration.py
+++ canvasapi/content_migration.py
@@ -3,10 +3,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from six import python_2_unicode_compatible
 
 from canvasapi.canvas_object import CanvasObject
-from canvasapi.grading_standard import GradingStandard
 from canvasapi.exceptions import CanvasException, RequiredFieldMissing
 from canvasapi.paginated_list import PaginatedList
-from canvasapi.rubric import Rubric
 from canvasapi.util import combine_kwargs, obj_or_id
 
 @python_2_unicode_compatible
