9b21728abd97d67a8ec050ffaf77942c2ac98d6b
Ralph Baird <ralph.baird@ucf.edu>
2018-04-30 17:32:44 -0400

    Added preliminary code for test_methods

--- tests/test_endpoint_docstrings.py
+++ tests/test_endpoint_docstrings.py
@@ -1,6 +1,23 @@
+import inspect
+import re
 def test_methods(*testMethods):
-    #For each:
-    #check if method contains an API call
-    #check if API call matches live documentation
-    #check if Mocker recieves a requst matching above two
+    failures = 0
+    for testMethod in testMethods:
+        # Check if docstring contains a calls line
+        if re.search(":calls:", inspect.getdoc(testMethod)):
+            callLine = re.search(":calls:[^_]*_", inspect.getdoc(testMethod)).group()
+            callGroups = re.search("(POST |GET |PUT |PATCH |DELETE )[^/]*(/[^ ]*)[^<]*<([^>]*)>",callLine).groups()
+            if checkUsage(callGroups) or checkAPIDoc(callGroups)
+                failures += 1
+
+    #TODO change to error codes based on results
+    return False
+
+#TODO implement
+def checkAPIDoc(groups):
+    verb, apiURL, docURL = groups
+    return False
+
+#TODO implement
+def checkUsage(groups):
     return False
