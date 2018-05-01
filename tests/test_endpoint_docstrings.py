7b325d475d7cba524b2b8a894d0e05c36130c949
Ralph Baird <ralph.baird@ucf.edu>
2018-05-01 14:13:03 -0400

    test_methods passes unit test

--- tests/test_endpoint_docstrings.py
+++ tests/test_endpoint_docstrings.py
@@ -1,23 +1,65 @@
 import inspect
 import re
-def test_methods(*testMethods):
-    failures = 0
-    for testMethod in testMethods:
-        # Check if docstring contains a calls line
-        if re.search(":calls:", inspect.getdoc(testMethod)):
-            callLine = re.search(":calls:[^_]*_", inspect.getdoc(testMethod)).group()
-            callGroups = re.search("(POST |GET |PUT |PATCH |DELETE )[^/]*(/[^ ]*)[^<]*<([^>]*)>",callLine).groups()
-            if checkUsage(callGroups) or checkAPIDoc(callGroups)
-                failures += 1
+import requests
 
-    #TODO change to error codes based on results
-    return False
+def test_methods(testMethod, quiet=False):
+    # Check if docstring contains a calls line; automatic pass if not
+    if re.search(":calls:", inspect.getdoc(testMethod)):
+        callLine = re.search(":calls:[^_]*_", inspect.getdoc(testMethod)).group()
+        docStringVerb, apiURL, docURL = re.search("(POST|GET|PUT|PATCH|DELETE)[^/]*(/[^ ]*)[^<]*<([^>]*)>",callLine).groups()
+        fileURL, endpointName = re.search("([^#]*)#(.*)",docURL).groups()
+        docResponse = requests.get(fileURL)
+        if docResponse.status_code != requests.codes.ok:
+            if not quiet:
+                print "%s Docstring URL request returned %d" % (testMethod.__name__, docResponse.status_code)
+            return False
 
-#TODO implement
-def checkAPIDoc(groups):
-    verb, apiURL, docURL = groups
-    return False
+        endpointHeading = re.search("name=[\'\"]%s[\'\"]" % endpointName, docResponse.text)
+        if not endpointName:
 
-#TODO implement
-def checkUsage(groups):
-    return False
+            if not quiet:
+                print "%s Docstring URL does not contain an endpoint name in link to API documentation" \
+                % testMethod.__name__
+            return False
+        if not endpointHeading:
+            if not quiet:
+                print "%s Docstring refers to %s in %s, not found" % (testMethod.__name__, endpointName, fileURL)
+            return False
+
+        endpointRegex = re.compile('<h3 class=[\"\']endpoint[\"\']>[\S\s]*<\/h3>')
+
+        endpointElement = endpointRegex.search(docResponse.text, endpointHeading.end())
+        if not endpointElement:
+            if not quiet:
+                print "Found no endpoint after %s in %s" % (endpointName, fileURL)
+            return False
+        docVerb, docEndpointURL = re.search("(POST|GET|PUT|PATCH|DELETE) (.*)", endpointElement.group()).groups()
+        if docVerb != docStringVerb:
+            if not quiet:
+                print "%s Docstring verb is %s, corresponding API documentation is %s" \
+                % (testMethod.__name__, docStringVerb, docVerb)
+            return False
+        if docEndpointURL != apiURL:
+
+            if not quiet:
+                print "%s Docstring API URL is %s, corresponding API documentation is %s" \
+                % (testMethod.__name__, apiURL, docEndpointURL)
+            return False
+
+        source = inspect.getsource(testMethod)
+        implementation = re.search("request\([\s\S]*[\'\"](POST|GET|PUT|PATCH|DELETE)[\'\"],[^\'\"]*[\'\"]([^\'\"]*)[\'\"]",source)
+        if not implementation:
+            if not quiet:
+                print s
+                print "%s Docstring refers to %s %s but implementation was not found." % (testMethod.__name__, docStringVerb, apiURL)
+            return False
+        if implementation.group(1) != docVerb:
+            if not quiet:
+                print "%s Docstring verb is %s but implementation is %s" % (testMethod.__name__, docStringVerb, implementation.group(1))
+            return False
+        apiShortURL = re.sub("\/api\/v[0-9]*\/","",apiURL)
+        if implementation.group(2) != re.sub(":[^\/]*","{}",apiShortURL):
+            if not quiet:
+                print "%s Docstring refers to %s URL but implementation is %s" %(testMethod.__name__, apiShortURL, implementation.group(2))
+            return False
+    return True
--- tests/test_test_endpoint_docstrings.py
+++ tests/test_test_endpoint_docstrings.py
@@ -6,17 +6,18 @@ from tests.test_endpoint_docstrings import test_methods
 
 class TestTestEndpointDocstrings(unittest.TestCase):
     def test_test_methods(self):
-        assert not test_methods(ExampleMethods.example_method_should_fail_online_documentation)
-        assert not test_methods(ExampleMethods.example_method_should_fail_implementation_verb)
-        assert not test_methods(ExampleMethods.example_method_should_fail_implementation_URL)
-        assert test_methods(ExampleMethods.example_method_should_pass_no_api_call)
-        assert test_methods(ExampleMethods.example_method_should_pass_all_correct)
+        assert not test_methods(ExampleMethods.example_method_should_fail_online_documentation, True)
+        assert not test_methods(ExampleMethods.example_method_should_fail_implementation_verb, True)
+        assert not test_methods(ExampleMethods.example_method_should_fail_implementation_URL, True)
+        assert test_methods(ExampleMethods.example_method_should_pass_no_api_call, True)
+        assert test_methods(ExampleMethods.example_method_should_pass_all_correct, True)
 
+#TODO REPLACE file:// with live documentation once local debugging is complete
 class ExampleMethods(CanvasObject):
     def example_method_should_fail_online_documentation(self):
         """
-        :calls: `DELETE /api/v1/files/:id  \
-        <https://canvas.instructure.com/doc/api/files.html#method.files.wrong>`_
+        :calls: `PUT /api/v1/files/:id  \
+        <https://canvas.instructure.com/doc/api/files.html#method.files.destroy>`_
 
         :rtype: :class:`canvasapi.file.File`
         """
