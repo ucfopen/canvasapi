c03943a9351cb41863f2b1c373d22d94536c00fc
Ralph Baird <ralph.baird@ucf.edu>
2018-05-03 15:32:30 -0400

    tests all canvasapi methods correctly

--- tests/test_endpoint_docstrings.py
+++ tests/test_endpoint_docstrings.py
@@ -1,84 +1,85 @@
 import inspect
 import re
 import requests
-import os
-import sys
+import canvasapi
 
 def test_method(testMethod, quiet=False):
     # Check if docstring contains a calls line; automatic pass if not
     if not inspect.getdoc(testMethod):
         return True
-    if re.search(":calls:", inspect.getdoc(testMethod)):
-        callGroups = re.search("`(POST|GET|PUT|PATCH|DELETE) ([\S<]*)[\S\s]*<([^>`]*)>?`_",inspect.getdoc(testMethod))
-        if not callGroups:
-            print "%s Syntax error in :calls: line: %s" % (inspect.getfile(testMethod) +" "+ testMethod.__name__, callLine)
-            return False
-        docStringVerb, apiURL, docURL = callGroups.groups()
-        fileURL, endpointName = re.search("([^#]*)#(.*)",docURL).groups()
-        docResponse = requests.get(fileURL)
-        if docResponse.status_code != requests.codes.ok:
-            if not quiet:
-                print "%s Docstring URL request returned %d" % (inspect.getfile(testMethod) +" "+ testMethod.__name__, docResponse.status_code)
-            return False
-
-        endpointHeading = re.search("name=[\'\"]%s[\'\"]" % endpointName, docResponse.text)
-        if not endpointName:
-
-            if not quiet:
-                print "%s Docstring URL does not contain an endpoint name in link to API documentation" \
-                % inspect.getfile(testMethod) +" "+ testMethod.__name__
-            return False
-        if not endpointHeading:
-            if not quiet:
-                print "%s Docstring refers to %s in %s, not found" % (inspect.getfile(testMethod) +" "+ testMethod.__name__, endpointName, fileURL)
-            return False
-
-        endpointRegex = re.compile('<h3 class=[\"\']endpoint[\"\']>[\S\s]*<\/h3>')
-        endpointStart = endpointHeading.end()
-        endpointEnd =
-        endpointElement = endpointRegex.search(docResponse.text, endpointHeading.end())
-        if not endpointElement:
-            if not quiet:
-                print "Found no endpoint after %s in %s" % (endpointName, fileURL)
-            return False
-        docVerb, docEndpointURL = re.search("(POST|GET|PUT|PATCH|DELETE) (.*)", endpointElement.group()).groups()
-        if docVerb != docStringVerb:
-            if not quiet:
-                print "%s Docstring verb is %s, corresponding API documentation is %s" \
-                % (inspect.getfile(testMethod) +" "+ testMethod.__name__, docStringVerb, docVerb)
-            return False
-        if docEndpointURL != apiURL:
-
-            if not quiet:
-                print "%s Docstring API URL is %s, corresponding API documentation is %s" \
-                % (inspect.getfile(testMethod) +" "+ testMethod.__name__, apiURL, docEndpointURL)
-            return False
+    if not re.search(":calls:", inspect.getdoc(testMethod)):
         return True
-        source = inspect.getsource(testMethod)
-        implementation = re.search("request\([\s\S]*[\'\"](POST|GET|PUT|PATCH|DELETE)[\'\"],[^\'\"]*[\'\"]([^\'\"]*)[\'\"]",source)
-        if not implementation:
-            if not quiet:
-                print "%s Docstring refers to %s %s but implementation was not found." % (inspect.getfile(testMethod) +" "+ testMethod.__name__, docStringVerb, apiURL)
-            return False
-        if implementation.group(1) != docVerb:
-            if not quiet:
-                print "%s Docstring verb is %s but implementation is %s" % (inspect.getfile(testMethod) +" "+ testMethod.__name__, docStringVerb, implementation.group(1))
-            return False
-        apiShortURL = re.sub("\/api\/v[0-9]*\/","",apiURL)
-        if implementation.group(2) != re.sub(":[^\/]*","{}",apiShortURL):
-            if not quiet:
-                print "%s Docstring refers to %s URL but implementation is %s" %(inspect.getfile(testMethod) +" "+ testMethod.__name__, apiShortURL, implementation.group(2))
+    callLines = re.findall("`(POST|GET|PUT|PATCH|DELETE)([^<]*)<([^>]*)>`_", inspect.getdoc(testMethod))
+    if len(callLines) == 0:
+        if not quiet:
+            print "%s Syntax error in :calls: line" % (inspect.getfile(testMethod) +" "+ testMethod.__name__)
+        return False
+    for callLine in callLines:
+        if not test_docString(testMethod, callLine, quiet):
             return False
     return True
 
-def test_methods():
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)),"canvasapi")
+def test_docString(testMethod, callLine, quiet):
+    docStringVerb, apiURL, docURL = callLine
+    apiURL = ''.join(apiURL.split())
+    if apiURL[-1] == '/':
+        apiURL = apiURL[0:-1]
+    fileURL, endpointName = re.search("([^#]*)#(.*)",docURL).groups()
+    docResponse = requests.get(fileURL)
+    if docResponse.status_code != requests.codes.ok:
+        if not quiet:
+            print "%s Docstring URL request returned %d" % (inspect.getfile(testMethod) +" "+ testMethod.__name__, docResponse.status_code)
+        return False
 
-    for py in [f[:-3] for f in os.listdir(path) if f.endswith('.py') and f != '__init__.py']:
-        mod = __import__('canvasapi.'+py)
-        classes = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
-        for cls in classes:
-            setattr(sys.modules[__name__], cls.__name__, cls)
-            methods = inspect.getmembers(cls, inspect.ismethod)
-            for methodname, method in methods:
-                test_method(method)
+    endpointHeading = re.search("name=[\'\"]%s[\'\"]" % endpointName, docResponse.text)
+    if not endpointName:
+
+        if not quiet:
+            print "%s Docstring URL does not contain an endpoint name in link to API documentation" \
+            % inspect.getfile(testMethod) +" "+ testMethod.__name__
+        return False
+    if not endpointHeading:
+        if not quiet:
+            print "%s Docstring refers to %s in %s, not found" % (inspect.getfile(testMethod) +" "+ testMethod.__name__, endpointName, fileURL)
+        return False
+
+    endpointRegex = re.compile('<h3 class=[\"\']endpoint[\"\']>[^<]*<\/h3>')
+    endpointStart = endpointRegex.search(docResponse.text, endpointHeading.end()).start()
+    endpointEndRegex = re.compile('<[^h\/]')
+    endpointEnd = endpointEndRegex.search(docResponse.text, endpointStart)
+    if not endpointEnd:
+        endpointEndPos = len(docReseponse.text)
+    else:
+        endpointEndPos = endpointEnd.start()
+    endpointElement = endpointRegex.search(docResponse.text, endpointStart, endpointEndPos)
+    endpointElements= []
+    while endpointElement:
+        endpointElements.append(endpointElement.group())
+        endpointElement = endpointRegex.search(docResponse.text, endpointElement.end(), endpointEndPos)
+    if len(endpointElements) == 0:
+        if not quiet:
+            print "Found no endpoint after %s in %s" % (endpointName, fileURL)
+        return False
+    docLines = []
+    for endpointElementStr in endpointElements:
+        docMatch = re.search("(POST|GET|PUT|PATCH|DELETE) (.*)", endpointElementStr)
+        docLines.append(docMatch.group())
+        docVerb, docEndpointURL = docMatch.groups()
+        if docVerb != docStringVerb:
+            continue
+        if docEndpointURL != apiURL:
+            continue
+        return True
+    if not quiet:
+        print "%s Docstring %s not found in API documentation (%s)" \
+            % (inspect.getfile(testMethod) +" "+ testMethod.__name__, docStringVerb + " " + apiURL, str(docLines))
+    return False
+
+def test_methods():
+    methods = set()
+    for _, module in inspect.getmembers(canvasapi, inspect.ismodule):
+        for _, theclass in inspect.getmembers(module, inspect.isclass):
+            for _, method in inspect.getmembers(theclass, inspect.ismethod):
+                methods.add(method)
+    for methodToTest in methods:
+        test_method(methodToTest)
--- tests/test_test_endpoint_docstrings.py
+++ tests/test_test_endpoint_docstrings.py
@@ -1,20 +1,20 @@
 import unittest
-import requests_mock
 from canvasapi.canvas_object import CanvasObject
-
 from tests.test_endpoint_docstrings import test_method, test_methods
 
+#test_endpoint_docstrings
 class TestTestEndpointDocstrings(unittest.TestCase):
     def test_test_method(self):
-        assert not test_method(ExampleMethods.example_method_should_fail_online_documentation, True)
-        #assert not test_method(ExampleMethods.example_method_should_fail_implementation_verb, True)
-        #assert not test_method(ExampleMethods.example_method_should_fail_implementation_URL, True)
-        assert test_method(ExampleMethods.example_method_should_pass_no_api_call, True)
-        assert test_method(ExampleMethods.example_method_should_pass_all_correct, True)
-        test_methods()
+        assert not test_method(ExampleMethods.fails_wrong_docstring_verb, True)
+        assert not test_method(ExampleMethods.fails_invalid_docstring_verb, True)
+        assert test_method(ExampleMethods.passes_no_api_call, True)
+        assert test_method(ExampleMethods.passes_good_docstring, True)
+        assert test_method(ExampleMethods.passes_multiple_endpoints, True)
+        assert test_method(ExampleMethods.passes_multiline_URL, True)
+        #test_methods()
 
 class ExampleMethods(CanvasObject):
-    def example_method_should_fail_online_documentation(self):
+    def fails_wrong_docstring_verb(self):
         """
         :calls: `PUT /api/v1/files/:id  \
         <https://canvas.instructure.com/doc/api/files.html#method.files.destroy>`_
@@ -27,43 +27,13 @@ class ExampleMethods(CanvasObject):
         )
         return ExampleMethods(self._requester, response.json())
 
-    def example_method_should_fail_implementation_verb(self):
-        """
-        Delete this file.
-
-        :calls: `DELETE /api/v1/files/:id  \
-        <https://canvas.instructure.com/doc/api/files.html#method.files.destroy>`_
-
-        :rtype: :class:`canvasapi.file.File`
-        """
-        response = self._requester.request(
-            'POST',
-            'files/{}'.format(self.id)
-        )
-        return ExampleMethods(self._requester, response.json())
-
-    def example_method_should_fail_implementation_URL(self):
-        """
-        Delete this file.
-
-        :calls: `DELETE /api/v1/files/:id  \
-        <https://canvas.instructure.com/doc/api/files.html#method.files.destroy>`_
-
-        :rtype: :class:`canvasapi.file.File`
-        """
-        response = self._requester.request(
-            'DELETE',
-            'fils/{}'.format(self.id)
-        )
-        return ExampleMethods(self._requester, response.json())
-
-    def example_method_should_pass_no_api_call(self):
+    def passes_no_api_call(self):
         """
         Empty docstring.
         """
         return False
 
-    def example_method_should_pass_all_correct(self):
+    def passes_good_docstring(self):
         """
         Delete this file.
 
@@ -77,3 +47,60 @@ class ExampleMethods(CanvasObject):
             'files/{}'.format(self.id)
         )
         return ExampleMethods(self._requester, response.json())
+
+    def passes_multiple_endpoints(self):
+        """
+        Return the details for a folder
+
+        :calls: `GET /api/v1/folders/:id \
+        <https://canvas.instructure.com/doc/api/files.html#method.folders.show>`_
+
+        :param folder: The object or ID of the folder to retrieve.
+        :type folder: :class:`canvasapi.folder.Folder` or int
+
+        :rtype: :class:`canvasapi.folder.Folder`
+        """
+        folder_id = obj_or_id(folder, "folder", (Folder,))
+
+        response = self.__requester.request(
+            'GET',
+            'folders/{}'.format(folder_id)
+        )
+        return Folder(self.__requester, response.json())
+
+    def fails_invalid_docstring_verb(self):
+        """
+        Delete this file.
+
+        :calls: `BELETE /api/v1/files/:id  \
+        <https://canvas.instructure.com/doc/api/files.html#method.files.destroy>`_
+
+        :rtype: :class:`canvasapi.file.File`
+        """
+        response = self._requester.request(
+            'DELETE',
+            'files/{}'.format(self.id)
+        )
+        return ExampleMethods(self._requester, response.json())
+
+    def passes_multiline_URL(self):
+        """
+        Fetch all preferences for the given communication channel.
+
+        :calls: `GET
+            /api/v1/users/:user_id/communication_channels/:communication_channel_id/ \
+                notification_preferences \
+        <https://canvas.instructure.com/doc/api/notification_preferences.html#method.notification_preferences.index>`_
+
+        :rtype: `list`
+        """
+        response = self._requester.request(
+            'GET',
+            'users/{}/communication_channels/{}/notification_preferences'.format(
+                self.user_id,
+                self.id
+            ),
+            _kwargs=combine_kwargs(**kwargs)
+        )
+
+        return response.json()['notification_preferences']
