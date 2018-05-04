e8ce9cee55ba89d75f9a2df4fcac16de6da1fa54
Ralph Baird <ralph.baird@ucf.edu>
2018-05-04 15:21:28 -0400

    resolved flake8 errors

--- tests/test_endpoint_docstrings.py
+++ tests/test_endpoint_docstrings.py
@@ -1,76 +1,96 @@
-import canvasapi
 import inspect
 import re
 import requests
 
+import canvasapi
 
-def test_method(testMethod, quiet=False):
+
+def test_method(themethod, quiet=False):
     # No docstring means no erroneous docstrings
-    if not inspect.getdoc(testMethod):
+    if not inspect.getdoc(themethod):
         return True
 
     # Docstrings without API calls can't be checked this way
-    if not re.search(":calls:", inspect.getdoc(testMethod)):
+    if not re.search(":calls:", inspect.getdoc(themethod)):
         return True
-    if not re.search("<\S*>", inspect.getdoc(testMethod)):
+    if not re.search("<\S*>", inspect.getdoc(themethod)):
         return True
 
-    method_string = inspect.getfile(testMethod) +" "+ testMethod.__name__
-    call_lines = re.findall("`(POST|GET|PUT|PATCH|DELETE)([^<]*)<([^>]*)>`_", inspect.getdoc(testMethod))
-    if len(call_lines) == 0:
+    method_string = inspect.getfile(themethod) + " " + themethod.__name__
+    call_lines = re.findall("`(POST|GET|PUT|PATCH|DELETE)([^<]*)<([^>]*)>`_",
+                            inspect.getdoc(themethod))
+    if not call_lines:
         if not quiet:
             # Docstring exists, has a :calls: line, contains a URL, but could
             # not be parsed;
             print "%s Failed to parse :calls: line." % (method_string)
         return False
     for call_line in call_lines:
-        if not test_docString(method_string, call_line, quiet):
+        if not test_docstring(method_string, call_line, quiet):
             return False
     return True
 
-def test_docString(method_string, call_line, quiet):
+
+def test_docstring(method_string, call_line, quiet):
     docstring_verb, api_URL, doc_URL = call_line
     api_URL = ''.join(api_URL.split())
     if api_URL[-1] == '/':
         api_URL = api_URL[0:-1]
-    docfile_URL, endpointName = re.search("([^#]*)#(.*)",doc_URL).groups()
+    docfile_URL, endpoint_name = re.search("([^#]*)#(.*)", doc_URL).groups()
     html_doc_response = requests.get(docfile_URL)
     if html_doc_response.status_code != requests.codes.ok:
         if not quiet:
-            print "%s docstring URL request returned %d" % (method_string, html_doc_response.status_code)
+            print "%s docstring URL request returned %d" % \
+                    (method_string, html_doc_response.status_code)
         return False
 
-    endpoint_h2 = re.search("name=[\'\"]%s[\'\"]" % endpointName, html_doc_response.text)
-    if not endpointName:
+    endpoint_h2 = re.search("name=[\'\"]%s[\'\"]" % endpoint_name,
+                            html_doc_response.text)
+    if not endpoint_name:
         if not quiet:
-            print "%s docstring URL does not contain an endpoint name in link to API documentation" \
-            % method_string
+            print "%s docstring URL does not contain an endpoint name in link \
+             to API documentation" % method_string
         return False
     if not endpoint_h2:
         if not quiet:
-            print "%s docstring refers to %s in %s, not found" % (method_string, endpointName, docfile_URL)
+            print "%s docstring refers to %s in %s, not found" \
+                    % (method_string, endpoint_name, docfile_URL)
         return False
 
-    endpoint_element_re = re.compile('<h3 class=[\"\']endpoint[\"\']>[^<]*<\/h3>')
-    endpoint_search_start_pos = endpoint_element_re.search(html_doc_response.text, endpoint_h2.end()).start()
+    endpoint_element_re = \
+        re.compile('<h3 class=[\"\']endpoint[\"\']>[^<]*<\/h3>')
+    endpoint_search_start_pos = \
+        endpoint_element_re.search(html_doc_response.text,
+                                   endpoint_h2.end()).start()
+
     after_endpoint_re = re.compile('<[^h\/]')
-    endpoint_search_end = after_endpoint_re.search(html_doc_response.text, endpoint_search_start_pos)
+    endpoint_search_end = after_endpoint_re.search(html_doc_response.text,
+                                                   endpoint_search_start_pos)
     if not endpoint_search_end:
         endpoint_search_stop_pos = len(html_doc_response.text)
     else:
         endpoint_search_stop_pos = endpoint_search_end.start()
-    endpoint_element_match = endpoint_element_re.search(html_doc_response.text, endpoint_search_start_pos, endpoint_search_stop_pos)
+    endpoint_element_match = \
+        endpoint_element_re.search(html_doc_response.text,
+                                   endpoint_search_start_pos,
+                                   endpoint_search_stop_pos)
     endpoint_element_list = []
     while endpoint_element_match:
         endpoint_element_list.append(endpoint_element_match.group())
-        endpoint_element_match = endpoint_element_re.search(html_doc_response.text, endpoint_element_match.end(), endpoint_search_stop_pos)
-    if len(endpoint_element_list) == 0:
+        endpoint_element_match = \
+            endpoint_element_re.search(html_doc_response.text,
+                                       endpoint_element_match.end(),
+                                       endpoint_search_stop_pos)
+
+    if not endpoint_element_list:
         if not quiet:
-            print "Found no endpoint after %s in %s" % (endpointName, docfile_URL)
+            print "Found no endpoint after %s in %s" % \
+                (endpoint_name, docfile_URL)
         return False
     docfile_lines = []
     for endpoint_element_str in endpoint_element_list:
-        docfile_match = re.search("(POST|GET|PUT|PATCH|DELETE) (.*)", endpoint_element_str)
+        docfile_match = re.search("(POST|GET|PUT|PATCH|DELETE) (.*)",
+                                  endpoint_element_str)
         docfile_lines.append(docfile_match.group())
         docfile_verb, docfile_API_URL = docfile_match.groups()
         if docfile_verb != docstring_verb:
@@ -80,8 +100,10 @@ def test_docString(method_string, call_line, quiet):
         return True
     if not quiet:
         print "%s docstring %s not found in %s (found %s)" \
-            % (method_string, docstring_verb + " " + api_URL, docfile_URL, str(docfile_lines))
+                % (method_string, docstring_verb + " " + api_URL, docfile_URL,
+                   str(docfile_lines))
     return False
+
 
 def test_methods():
     methods = set()
@@ -89,5 +111,5 @@ def test_methods():
         for _, theclass in inspect.getmembers(module, inspect.isclass):
             for _, method in inspect.getmembers(theclass, inspect.ismethod):
                 methods.add(method)
-    for methodToTest in methods:
-        test_method(methodToTest)
+    for method_to_test in methods:
+        test_method(method_to_test)
