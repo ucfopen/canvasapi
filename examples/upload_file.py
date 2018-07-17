3d966b191b9ba5f91b43a9bd8973410d4e00a074
Markus <markus@cygnul.com>
2018-07-17 13:44:54 -0500

    Added some more examples
    Added an example to dump all course content. So if you want a complete archive of the course you took/taught, it will download it. This is a pain right now. Need to do more research.

--- examples/upload_file.py
+++ examples/upload_file.py
@@ -8,11 +8,12 @@ API_KEY = open(".canvas.txt").read().strip()
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
-course_id = 1113938
+course_id = 1113938 # Analog Electronics
 course = canvas.get_course(course_id)
 fh = open('HW10v2simpleGenCEamp.pdf')
-
-hw_folder = [x for x in course.get_folders() if x.id == 5816809][0]
+target_folder_id = 5816809
+hw_folder = [x for x in course.get_folders() if x.id == target_folder_id][0]
+print(dir(hw_folder))
 hw_folder.upload(fh)
 
 
