fff6f73294b16b300e79a2cfe22eb85dfb3f1986
William James Wrbican <wjw27@ACCESS.PSU.EDU>
2018-12-04 15:26:02 -0500

    add kwargs to section.edit()

--- canvasapi/section.py
+++ canvasapi/section.py
@@ -104,7 +104,7 @@ class Section(CanvasObject):
         )
         return Section(self._requester, response.json())
 
-    def edit(self):
+    def edit(self, **kwargs):
         """
         Edit contents of a target section.
 
@@ -115,7 +115,9 @@ class Section(CanvasObject):
         """
         response = self._requester.request(
             'PUT',
-            'sections/{}'.format(self.id)
+            'sections/{}'.format(self.id),
+            _kwargs=combine_kwargs(**kwargs)
+
         )
         return Section(self._requester, response.json())
 
