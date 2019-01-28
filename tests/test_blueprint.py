6f542c4b37e43ba974148897f1c0d1c892d66080
Gabriela Dijkhoffz <gabriela.dijkhoffz@knights.ucf.edu>
2019-01-28 16:56:10 -0500

    Added and completed tests for get_unsynced_changes, list_blueprint_migrations, show_blueprint_migration, and get_migration_details.

--- tests/test_blueprint.py
+++ tests/test_blueprint.py
@@ -3,7 +3,10 @@ import unittest
 import requests_mock
 
 from canvasapi import Canvas
+from canvasapi.blueprint import BlueprintMigration
+from canvasapi.blueprint import ChangeRecord
 from canvasapi.course import Course
+from canvasapi.paginated_list import PaginatedList
 from tests import settings
 from tests.util import register_uris
 
@@ -51,5 +54,50 @@ class TestBlueprint(unittest.TestCase):
     # change_blueprint_restrictions()
     def test_change_blueprint_restrictions(self, m):
         register_uris({'blueprint': ['change_blueprint_restrictions']}, m)
-        blueprint_restriction = self.blueprint.change_blueprint_restrictions()
-        self.assertTrue(blueprint_restriction)
+        blueprint_restriction = self.blueprint.change_blueprint_restrictions("quiz", 1, True)
+        self.assertIsInstance(blueprint_restriction, bool)
+
+    # get_unsynced_changes()
+    def test_get_unsynced_changes(self, m):
+        register_uris({'blueprint': ['get_unsynced_changes']}, m)
+        unsynced_changes = self.blueprint.get_unsynced_changes()
+        self.assertIsInstance(unsynced_changes, PaginatedList)
+        self.assertIsInstance(unsynced_changes[0], ChangeRecord)
+        self.assertEqual(unsynced_changes[0].asset_id, 1)
+        self.assertEqual(unsynced_changes[0].asset_type, "quiz")
+        self.assertEqual(unsynced_changes[0].asset_name, "test quiz")
+        self.assertEqual(unsynced_changes[0].change_type, "updated")
+
+    # list_blueprint_migrations()
+    def test_list_blueprint_migrations(self, m):
+        register_uris({'blueprint': ['list_blueprint_migrations']}, m)
+        blueprint_migrations = self.blueprint.list_blueprint_migrations()
+        self.assertIsInstance(blueprint_migrations, PaginatedList)
+        self.assertIsInstance(blueprint_migrations[0], BlueprintMigration)
+        self.assertEqual(blueprint_migrations[0].id, 1)
+        self.assertEqual(blueprint_migrations[0].user_id, 1)
+        self.assertEqual(blueprint_migrations[0].template_id, 1)
+
+    # show_blueprint_migration()
+    def test_show_blueprint_migration(self, m):
+        register_uris({'blueprint': ['show_blueprint_migration']}, m)
+        blueprint_migration = self.blueprint.show_blueprint_migration(1)
+        self.assertIsInstance(blueprint_migration, BlueprintMigration)
+        self.assertEqual(blueprint_migration.id, 1)
+        self.assertEqual(blueprint_migration.user_id, 1)
+        self.assertEqual(blueprint_migration.workflow_state, "completed")
+        self.assertEqual(blueprint_migration.template_id, 1)
+
+    def test_get_migration_details(self, m):
+        register_uris({'blueprint': ['get_migration_details']}, m)
+        migration_details = self.blueprint.get_migration_details()
+        self.assertIsInstance(migration_details, PaginatedList)
+        self.assertIsInstance(migration_details[0], ChangeRecord)
+        self.assertEqual(migration_details[0].asset_id, 1)
+        self.assertEqual(migration_details[0].asset_type, "assignment")
+        self.assertEqual(migration_details[0].asset_name, "Test Assignment")
+        self.assertEqual(migration_details[0].locked, True)
+        self.assertEqual(migration_details[1].asset_id, 2)
+        self.assertEqual(migration_details[1].asset_type, "quiz")
+        self.assertEqual(migration_details[1].asset_name, "Test Quiz")
+        self.assertEqual(migration_details[1].locked, False)
