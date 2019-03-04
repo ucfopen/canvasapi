57292e47249857f63db60a0d5415c0797a918544
Jesse McBride <jm@ucf.edu>
2019-03-04 13:38:44 -0500

    Remove legacy CI files

--- cmd_run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run --rm -v $(pwd):/app canvasapi coverage run -m unittest discover
-docker run --rm -v $(pwd):/app canvasapi coverage xml
-docker run --rm -v $(pwd):/app canvasapi pycodestyle canvasapi tests
-docker run --rm -v $(pwd):/app canvasapi pyflakes canvasapi tests
