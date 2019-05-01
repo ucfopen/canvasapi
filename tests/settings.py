efda17bb265e2bc69752370e7a539fc41ccb1b94
Gabriela Dijkhoffz <gabriela.dijkhoffz@knights.ucf.edu>
2019-05-01 14:16:11 -0400

    Changed an invalid URL variable

--- tests/settings.py
+++ tests/settings.py
@@ -4,7 +4,7 @@ BASE_URL = 'https://example.com'
 BASE_URL_WITH_VERSION = 'https://example.com/api/v1/'
 BASE_URL_AS_HTTP = 'http://example.com'
 BASE_URL_AS_BLANK = ''
-BASE_URL_AS_INVALID = 'google.com'
+BASE_URL_AS_INVALID = 'example.com'
 API_KEY = '123'
 
 INVALID_ID = 9001
