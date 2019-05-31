35f1d14ef66342eb186aee6d9ffc2b536e6bc9d1
Jose Silveti <jose.silveti@ucf.edu>
2019-05-31 12:58:33 -0400

    Added function signature for grading period update inside the grading period class.

--- canvasapi/grading_period.py
+++ canvasapi/grading_period.py
@@ -10,3 +10,13 @@ class GradingPeriod(CanvasObject):
 
     def __str__(self):
         return '{} ({})'.format(self.title, self.id)
+
+    def update(self, **kwargs):
+        """
+        Update a grading period for a course.
+
+        :calls: `PUT /api/v1/courses/:course_id/grading_periods/:id \
+        <https://canvas.instructure.com/doc/api/grading_periods.html#method.grading_periods.update>`_
+
+        :rtype: :class:`canvasapi.grading_period.GradingPeriod`
+        """
