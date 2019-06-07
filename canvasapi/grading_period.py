324158fe61741fe49be241e089664c0e30041808
Jose Silveti <jose.silveti@ucf.edu>
2019-06-07 12:55:08 -0400

    Added course_id parameter to both the delete and update method of the grading period API. Changed tests accordingly. Found a typo in the code of the update function which was leading to an error.

--- canvasapi/grading_period.py
+++ canvasapi/grading_period.py
@@ -12,53 +12,54 @@ class GradingPeriod(CanvasObject):
     def __str__(self):
         return '{} ({})'.format(self.title, self.id)
 
-    def update(self, grading_period, **kwargs):
+    def update(self, course_id, grading_period, **kwargs):
         """
         Update a grading period for a course.
 
         :calls: `PUT /api/v1/courses/:course_id/grading_periods/:id \
         <https://canvas.instructure.com/doc/api/grading_periods.html#method.grading_periods.update>`_
 
+        :param course_id: Id for course of grading period which is to be changed.
+        :type: int
+
         :param grading_period: List of Nester paramameters which requires the
         start_date and end_date parameters.
+        :type grading_period: list[dict]
 
         :rtype: :class:`canvasapi.grading_period.GradingPeriod`
         """
-        if not hasattr(self, 'course_id'):
-            raise ValueError('Can only update grading periods from a Course.')
-
         if isinstance(grading_period, list):
-            kwargs['grading_period'] = grading_period
+            kwargs['grading_periods'] = grading_period
         else:
             raise RequiredFieldMissing("List is required")
 
         response = self._requester.request(
             'PUT',
-            'courses/{}/grading_periods/{}'.format(self.course_id, self.id),
+            'courses/{}/grading_periods/{}'.format(course_id, self.id),
             _kwargs=combine_kwargs(**kwargs)
         )
+
         response_json = response.json()
-        response_json.update({'course_id': self.course_id})
+        response_json.update({'course_id': course_id})
 
         return GradingPeriod(self._requester, response_json['grading_periods'][0])
 
-    def delete(self, **kwargs):
+    def delete(self, course_id, **kwargs):
         """
         Delete a grading period for a course.
 
         :calls: `DELETE /api/v1/courses/:course_id/grading_periods/:id \
         <https://canvas.instructure.com/doc/api/grading_periods.html#method.grading_periods.update>`_
 
+        :param course_id: Id for course of grading period which is to be deleted.
+        :type: int
+
         :returns: True if the grading period was deleted, False otherwise.
         :rtype: bool
         """
-
-        if not hasattr(self, 'course_id'):
-            raise ValueError('Need a course id to delete a grading period from a course')
-
         response = self._requester.request(
             'DELETE',
-            'courses/{}/grading_periods/{}'.format(self.course_id, self.id),
+            'courses/{}/grading_periods/{}'.format(course_id, self.id),
             _kwargs=combine_kwargs(**kwargs)
         )
 
--- tests/test_grading_period.py
+++ tests/test_grading_period.py
@@ -17,7 +17,7 @@ class TestGradingPeriod(unittest.TestCase):
 
         self.grading_period = GradingPeriod(
             self.canvas._Canvas__requester,
-            {"title": "grading period 1", "id": "1", "course_id": 1}
+            {"title": "grading period 1", "id": "1"}
         )
 
     def test_str(self, m):
@@ -29,8 +29,7 @@ class TestGradingPeriod(unittest.TestCase):
     def test_update(self, m):
         register_uris({'grading_period': ['update']}, m)
 
-        edited_grading_period = self.grading_period.update(
-            grading_period=[{'start_date': '2019-06-10', 'end_date': '2019-06-15'}])
+        edited_grading_period = self.grading_period.update(1, grading_period=[{'start_date': '2019-06-10', 'end_date': '2019-06-15'}])
 
         self.assertIsInstance(edited_grading_period, GradingPeriod)
         self.assertTrue(hasattr(edited_grading_period, 'title'))
@@ -43,4 +42,4 @@ class TestGradingPeriod(unittest.TestCase):
     # delete()
     def test_delete(self, m):
         register_uris({'grading_period': ['delete']}, m)
-        self.assertTrue(self.grading_period.delete())
+        self.assertTrue(self.grading_period.delete(1))
