17b076425f4b83cbf60019df7aaba6a680e9f9e3
Davis Goff <davis.goff@ucf.edu>
2019-06-12 08:18:28 -0400

    Added get choice functions

--- canvasapi/poll.py
+++ canvasapi/poll.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from six import python_2_unicode_compatible
 
 from canvasapi.canvas_object import CanvasObject
-from canvasapi.util import combine_kwargs
+from canvasapi.util import combine_kwargs, obj_or_id
+from canvasapi.paginated_list import PaginatedList
+from canvasapi.poll_choice import PollChoice
 
 
 @python_2_unicode_compatible
@@ -31,3 +33,39 @@ class Poll(CanvasObject):
             _kwargs=combine_kwargs(**kwargs)
         )
         return Poll(self._requester, response.json())
+
+    def get_choices(self, **kwargs):
+        """
+        Returns a paginated list of PollChoices of a poll, based on poll id.
+
+        :calls: `GET /api/v1/polls/:poll_id/poll_choices \
+        <https://canvas.instructure.com/doc/api/poll_choices.html#method.polling/poll_choices.index>`_
+
+        :rtype: :class:`canvasapi.paginated_list.PaginatedList` of
+            :class:`canvasapi.poll_choice.PollChoice`
+        """
+        return PaginatedList(
+            PollChoice,
+            self.__requester,
+            'GET',
+            'polls/{}/poll_choices'.format(self.id),
+            _kwargs=combine_kwargs(**kwargs)
+        )
+
+    def get_choice(self, poll_choice, **kwargs):
+        """
+        Returns the poll choice with the given id.
+
+        :calls: `GET /api/v1/polls/:poll_id/poll_choices/:id \
+        <https://canvas.instructure.com/doc/api/poll_choices.html#method.polling/poll_choices.show>`_
+
+        :rtype: :class:`canvasapi.poll_choice.PollChoice`
+        """
+        poll_choice_id = obj_or_id(poll_choice, "poll_choice", (PollChoice,))
+
+        response = self._requester.request(
+            'GET',
+            'polls/{}/poll_choices/{}'.format(self.id, poll_choice_id),
+            _kwargs=combine_kwargs(**kwargs)
+        )
+        return PollChoice(self._requester, response.json())
