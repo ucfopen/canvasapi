59df82357dd70e096128a8c26aa1a6ac29c32bc2
Weining Li <weining.li@ucf.edu>
2019-06-12 08:44:14 -0400

    issue/254 - added content exports

--- canvasapi/course.py
+++ canvasapi/course.py
@@ -2347,6 +2347,71 @@ class Course(CanvasObject):
             kwargs=combine_kwargs(**kwargs)
         )
 
+    def list_content_exports(self, **kwargs):
+        """
+        Return a paginated list of the past and pending content export jobs for a course.
+
+        :calls: `GET /api/v1/courses/:course_id/content_exports\
+        <https://canvas.instructure.com/doc/api/content_exports.html#method.content_exports_api.index>`_
+
+        :rtype: :class:`canvasapi.paginated_list.PaginatedList` of
+            :class:`canvasapi.content_export.ContentExport`
+        """
+        from canvasapi.content_export import ContentExport
+
+        return PaginatedList(
+            ContentExport,
+            self._requester,
+            'GET',
+            'courses/{}/content_exports'.format(
+                self.id
+            ),
+            kwargs=combine_kwargs(**kwargs)
+        )
+
+    def show_content_export(self, export_id, **kwargs):
+        """
+        Return information about a single content export.
+
+        :calls: `GET /api/v1/courses/:course_id/content_exports/:id\
+        <https://canvas.instructure.com/doc/api/content_exports.html#method.content_exports_api.show>`_
+
+        :rtype: :class:`canvasapi.content_export.ContentExport`
+        """
+        from canvasapi.content_export import ContentExport
+
+        response = self._requester.request(
+            'GET',
+            'courses/{}/content_exports/{}'.format(
+                self.id,
+                export_id
+            ),
+            _kwargs=combine_kwargs(**kwargs)
+        )
+        return ContentExport(self._requester, response.json())
+
+    def export_content(self, export_type, **kwargs):
+        """
+        Begin a content export job for a course.
+
+        :calls: `POST /api/v1/courses/:course_id/content_exports\
+        <https://canvas.instructure.com/doc/api/content_exports.html#method.content_exports_api.create>`_
+
+        :rtype: :class:`canvasapi.content_export.ContentExport`
+        """
+        from canvasapi.content_export import ContentExport
+
+        kwargs['export_type'] = export_type
+
+        response = self._requester.request(
+            'POST',
+            'courses/{}/content_exports'.format(
+                self.id,
+            ),
+            _kwargs=combine_kwargs(**kwargs)
+        )
+        return ContentExport(self._requester, response.json())
+
 
 @python_2_unicode_compatible
 class CourseNickname(CanvasObject):
