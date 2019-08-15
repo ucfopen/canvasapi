64714b844941b983cea364e9d99c14158f71b591
Jose Silveti <jose.silveti@ucf.edu>
2019-08-15 12:54:32 -0400

    Added collab class and method

---
+++ canvasapi/collaboration.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from six import python_2_unicode_compatible
+
+from canvasapi.canvas_object import CanvasObject
+from canvasapi.exceptions import RequiredFieldMissing
+from canvasapi.util import combine_kwargs
+
+
+@python_2_unicode_compatible
+class Collaboration(CanvasObject):
+    def __str__(self):
+        return "{} ({})".format(self.collaboration_type, self.id)
