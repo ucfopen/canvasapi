e4fb86acccd7bc233f47ccff01d147079416d10b
Jose Silveti <jose.silveti@ucf.edu>
2019-08-16 11:37:09 -0400

    Added function for getting collaborators, added tests and fixtures

--- tests/test_collaboration.py
+++ tests/test_collaboration.py
@@ -4,8 +4,10 @@ import unittest
 import requests_mock
 
 from canvasapi import Canvas
-from canvasapi.collaboration import Collaboration
+from canvasapi.collaboration import Collaboration, Collaborator
+from canvasapi.paginated_list import PaginatedList
 from tests import settings
+from tests.util import cleanup_file, register_uris
 
 
 @requests_mock.Mocker()
@@ -35,4 +37,29 @@ class TestCollaboration(unittest.TestCase):
 
     def test_str(self, m):
         test_str = str(self.collaboration)
+        self.assertIsInstance(test_str, str)
+
+    def test_get_collaborators(self, m):
+        register_uris({"collaboration": ["get_collaborators"]}, m)
+
+        collaborator_list = self.collaboration.get_collaborators()
+
+        self.assertIsInstance(collaborator_list, PaginatedList)
+
+
+@requests_mock.Mocker()
+class TestCollaborator(unittest.TestCase):
+    def setUp(self):
+        self.canvas = Canvas(settings.BASE_URL, settings.API_KEY)
+
+        self.collaborator = Collaborator(
+            self.canvas._Canvas__requester,
+            {
+                "id": 12345,
+                "type": "user",
+                "name": "Don Draper"
+            })
+
+    def test_str(self, m):
+        test_str = str(self.collaborator)
         self.assertIsInstance(test_str, str)
