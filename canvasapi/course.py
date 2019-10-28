46c3022c5f13f67c60bad0b19183c2a28c082765
ctcuff <dev.ctcuff@gmail.com>
2019-10-28 13:44:30 -0400

    Fixed incorrect return type in doc string

--- canvasapi/course.py
+++ canvasapi/course.py
@@ -1235,7 +1235,7 @@ class Course(CanvasObject):
         <https://canvas.instructure.com/doc/api/grade_change_log.html#method.grade_change_audit_api.for_course>`
 
         :rtype: :class:`canvasapi.paginated_list.PaginatedList` of
-            :class:`canvasapi.grade_change_event.GradingPeriod`
+            :class:`canvasapi.grade_change_event.GradeChangeEvent`
         """
 
         return PaginatedList(
