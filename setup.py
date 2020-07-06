97c8c1dd3dbd6509b976a018f0e0420957b847c5
Matthew Emond <me@ucf.edu>
2020-07-06 11:26:28 -0400

    Removed support for 3.5. Added to changelog

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
