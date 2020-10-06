64ffd56befb9509ff0e4feed57d6a0bff1577cd6
bennettscience <brian.bennett2@gmail.com>
2020-10-05 22:20:28 -0400

    Refactor for more flexible arguments
    See https://github.com/ucfopen/canvasapi/pull/421#discussion_r499841262
    
    The requester can handle `Course` objects or plain strings. Handling both
    makes it easier on the user because they can reuse a course object they may
    already have. Canvas just looks for the `context_code` string.
    
    The `context_codes` param is required by the API. The `get_announcements` function
    requires a list of either strings or `Course` objects to request. Formatting
    is done by the library before sending to the `Announcements` endpoint.

--- canvasapi/canvas.py
+++ canvasapi/canvas.py
@@ -437,32 +437,36 @@ class Canvas(object):
         )
         return response.json()
 
-    def get_announcements(self, course_list, **kwargs):
+    def get_announcements(self, courses, **kwargs):
         """
         List announcements.
 
         :calls: `GET /api/v1/announcements \
         <https://canvas.instructure.com/doc/api/announcements.html#method.announcements_api.index>`_
 
-        :param course_list: Course ID(s) to request announcements from.
-        :type course_list: list
+        :param courses: Course ID(s) or <Course> objects to request announcements from.
+        :type courses: list
 
         :rtype: :class:`canvasapi.paginated_list.PaginatedList` of
                 :class:`canvasapi.discussion_topic.DiscussionTopic`
         """
         from canvasapi.discussion_topic import DiscussionTopic
 
-        if type(course_list) is not list:
+        if type(courses) is not list or len(courses) == 0:
             raise RequiredFieldMissing("Course IDs need to be passed as a list")
 
-        course_ids = [f"course_{course_id}" for course_id in course_list]
+        # The type of object in `courses` is taken care of by obj_or_id, extracting the couse
+        # ID from a <Course> object or by returning plain strings.
+        course_ids = [obj_or_id(course_id, "course_id", (Course,)) for course_id in courses]
+
+        # Set the **kwargs object vaue so it can be combined with others passed by the user.
+        kwargs['context_codes'] = [f"course_{course_id}" for course_id in course_ids]
 
         return PaginatedList(
             DiscussionTopic,
             self.__requester,
             "GET",
             "announcements",
-            context_codes=course_ids,
             _kwargs=combine_kwargs(**kwargs),
         )
 
