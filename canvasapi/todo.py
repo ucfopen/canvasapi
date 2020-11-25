7d6b7c3bc2da6a2360eb766ccfe872bbfb25f5e3
Matthew Emond <me@ucf.edu>
2020-11-25 15:31:18 -0500

    Add test for Todo __str__. Update __str__ to use type instead of id (which doesn't exist)

--- canvasapi/todo.py
+++ canvasapi/todo.py
@@ -3,4 +3,4 @@ from canvasapi.canvas_object import CanvasObject
 
 class Todo(CanvasObject):
     def __str__(self):
-        return "Todo List ({})".format(self.id)
+        return "Todo Item ({})".format(self.type)
