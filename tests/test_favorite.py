37c753083cc089b97af3bbec50f6f694cd7fc533
deundrewilliams <de871959@ucf.edu>
2021-01-11 11:37:20 -0500

    Moves creation of favorite objects out of setUp()

--- tests/test_favorite.py
+++ tests/test_favorite.py
@@ -13,6 +13,9 @@ class TestFavorite(unittest.TestCase):
     def setUp(self):
         self.canvas = Canvas(settings.BASE_URL, settings.API_KEY)
 
+    # __str__()
+    def test_uncapitalized___str__(self, m):
+
         with requests_mock.Mocker() as m:
             requires = {
                 "current_user": [
@@ -27,8 +30,29 @@ class TestFavorite(unittest.TestCase):
             self.favorite_course = self.user.add_favorite_course(1)
             self.favorite_group = self.user.add_favorite_group(1)
 
-    # __str__()
-    def test__str__(self, m):
+        string = str(self.favorite_course)
+        self.assertIsInstance(string, str)
+
+        string = str(self.favorite_group)
+        self.assertIsInstance(string, str)
+
+    def test_capitalized___str__(self, m):
+
+        with requests_mock.Mocker() as m:
+            requires = {
+                "current_user_capitalized_context_types": [
+                    "add_favorite_course",
+                    "add_favorite_group",
+                    "get_by_id",
+                ]
+            }
+            register_uris(requires, m)
+
+            self.user = self.canvas.get_current_user()
+            self.favorite_course = self.user.add_favorite_course(1)
+            self.favorite_group = self.user.add_favorite_group(1)
+
+
         string = str(self.favorite_course)
         self.assertIsInstance(string, str)
 
@@ -37,33 +61,87 @@ class TestFavorite(unittest.TestCase):
 
     # remove()
     def test_remove_uncapitalized_favorite_course(self, m):
-        register_uris({"current_user": ["remove_favorite_course"]}, m)
 
-        evnt = self.favorite_course.remove()
+        with requests_mock.Mocker() as m:
+            requires = {
+                "current_user": [
+                    "add_favorite_course",
+                    "get_by_id",
+                ]
+            }
+            register_uris(requires, m)
+
+            self.user = self.canvas.get_current_user()
+            self.favorite_course = self.user.add_favorite_course(1)
+
+            register_uris({"current_user": ["remove_favorite_course"]}, m)
+
+            evnt = self.favorite_course.remove()
+
         self.assertIsInstance(evnt, Favorite)
         self.assertEqual(evnt.context_type, "course")
         self.assertEqual(evnt.context_id, 1)
 
     def test_remove_uncapitalized_favorite_group(self, m):
-        register_uris({"current_user": ["remove_favorite_group"]}, m)
 
-        evnt = self.favorite_group.remove()
+        with requests_mock.Mocker() as m:
+            requires = {
+                "current_user": [
+                    "add_favorite_group",
+                    "get_by_id",
+                ]
+            }
+            register_uris(requires, m)
+
+            self.user = self.canvas.get_current_user()
+            self.favorite_group = self.user.add_favorite_group(1)
+
+            register_uris({"current_user": ["remove_favorite_group"]}, m)
+
+            evnt = self.favorite_group.remove()
+
         self.assertIsInstance(evnt, Favorite)
         self.assertEqual(evnt.context_type, "group")
         self.assertEqual(evnt.context_id, 1)
 
     def test_remove_capitalized_favorite_course(self, m):
-        register_uris({"current_user_upper_favorite": ["remove_favorite_course"]}, m)
 
-        evnt = self.favorite_course.remove()
+        with requests_mock.Mocker() as m:
+            requires = {
+                "current_user_capitalized_context_types": [
+                    "add_favorite_course",
+                    "get_by_id",
+                ]
+            }
+            register_uris(requires, m)
+
+            self.user = self.canvas.get_current_user()
+            self.favorite_course = self.user.add_favorite_course(1)
+
+            register_uris({"current_user_capitalized_context_types": ["remove_favorite_course"]}, m)
+
+            evnt = self.favorite_course.remove()
         self.assertIsInstance(evnt, Favorite)
         self.assertEqual(evnt.context_type, "Course")
         self.assertEqual(evnt.context_id, 1)
 
     def test_remove_capitalized_favorite_group(self, m):
-        register_uris({"current_user_upper_favorite": ["remove_favorite_group"]}, m)
 
-        evnt = self.favorite_group.remove()
+        with requests_mock.Mocker() as m:
+            requires = {
+                "current_user_capitalized_context_types": [
+                    "add_favorite_group",
+                    "get_by_id",
+                ]
+            }
+            register_uris(requires, m)
+
+            self.user = self.canvas.get_current_user()
+            self.favorite_group = self.user.add_favorite_group(1)
+
+            register_uris({"current_user_capitalized_context_types": ["remove_favorite_group"]}, m)
+
+            evnt = self.favorite_group.remove()
         self.assertIsInstance(evnt, Favorite)
         self.assertEqual(evnt.context_type, "Group")
         self.assertEqual(evnt.context_id, 1)
