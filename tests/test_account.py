42d5b78ba2a898fcff2b00add1ef34eff671a47f
shaneros <shaneros@gmail.com>
2021-12-23 15:58:33 -0600

    Tests and fixtures for delete admin

--- tests/test_account.py
+++ tests/test_account.py
@@ -153,8 +153,35 @@ class TestAccount(unittest.TestCase):
             self.account.delete()
 
     # delete_admin()
-    def test_delete_admin(self, m):
-        return
+    def test_delete_admin_id(self, m):
+        register_uris({"account": ["delete_admin"]}, m)
+
+        deleted_admin = self.account.delete_admin(self.user.id)
+
+        self.assertIsInstance(deleted_admin, Admin)
+        self.assertTrue(hasattr(deleted_admin, "id"))
+        self.assertTrue(hasattr(deleted_admin, "role"))
+        self.assertTrue(hasattr(deleted_admin, "role_id"))
+        self.assertTrue(hasattr(deleted_admin, "workflow_state"))
+        self.assertEqual(deleted_admin.user["login_id"], "jdoe")
+        self.assertEqual(deleted_admin.role, "AccountAdmin")
+        self.assertEqual(deleted_admin.role_id, 1)
+        self.assertEqual(deleted_admin.workflow_state, "deleted")
+
+    def test_delete_admin_obj(self, m):
+        register_uris({"account": ["delete_admin"]}, m)
+
+        deleted_admin = self.account.delete_admin(self.user)
+
+        self.assertIsInstance(deleted_admin, Admin)
+        self.assertTrue(hasattr(deleted_admin, "id"))
+        self.assertTrue(hasattr(deleted_admin, "role"))
+        self.assertTrue(hasattr(deleted_admin, "role_id"))
+        self.assertTrue(hasattr(deleted_admin, "workflow_state"))
+        self.assertEqual(deleted_admin.user["login_id"], "jdoe")
+        self.assertEqual(deleted_admin.role, "AccountAdmin")
+        self.assertEqual(deleted_admin.role_id, 1)
+        self.assertEqual(deleted_admin.workflow_state, "deleted")
 
     # delete_user()
     def test_delete_user_id(self, m):
