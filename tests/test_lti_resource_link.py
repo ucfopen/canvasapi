dd38ca954d3b974c1bf8f3db221f71ada5c264b4
Jasmine Hou <jsmnhou@umich.edu>
2024-12-09 16:17:33 -0500

    bruh

--- tests/test_lti_resource_link.py
+++ tests/test_lti_resource_link.py
@@ -3,7 +3,6 @@ import unittest
 import requests_mock
 
 from canvasapi import Canvas
-from canvasapi.exceptions import RequiredFieldMissing
 from tests import settings
 from tests.util import register_uris
 
